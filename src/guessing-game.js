1979b4aba8b230cb4346b660e942342b50bf854c
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-02-23 15:32:32 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    //сохраним значения в объект для работы
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    // предлагаем кандидата из середины диапвзона
+    const candidate = Math.ceil(this.min + (this.max - this.min) / 2); // c Math.floor тесты не проходит)
+    this.candidate = candidate;
+    console.log("Кандидат:", this.candidate);
+    return candidate;
+  }
 
-    guess() {
+  lower() {
+    // если оказались тут число больше предложенного candidate
+    // следовательно можно записать этого кандидата в this.min
+    this.max = this.candidate;
+    console.log("lower()", "Min:", this.min, "Max:", this.max);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    // если оказались тут число меньше предложенного candidate
+    // следовательно можно записать этого кандидата в this.max
+    this.min = this.candidate;
+    console.log("greater()", "Min:", this.min, "Max:", this.max);
+  }
 }
 
 module.exports = GuessingGame;
